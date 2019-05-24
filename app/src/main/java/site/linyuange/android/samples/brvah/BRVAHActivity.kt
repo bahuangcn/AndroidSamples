9cd2ccee0606a9b89afcd3554f5e218a4bfa2f70
八荒 <bahuangcn@gmail.com>
2019-05-24 17:05:09 +0800

    add dependencies BaseRecyclerViewAdapterHelper

---
+++ app/src/main/java/site/linyuange/android/samples/brvah/BRVAHActivity.kt
@@ -0,0 +1,14 @@
+package site.linyuange.android.samples.brvah
+
+import site.linyuange.android.samples.R
+import site.linyuange.framework.BaseActivity
+
+/**
+ * Author: BaHuang
+ * Date: 2019/5/24 13:17
+ */
+class BRVAHActivity : BaseActivity() {
+    override fun getLayoutRes(): Int {
+        return R.layout.activity_brvah
+    }
+}
